a11bbb8565b6f374d20f190b7db342f912723082
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-09-12 03:10:51 -0400

    added function addStar array to ass stars

--- main.js
+++ main.js
@@ -37,6 +37,18 @@ scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function addStar() {
+    const geometry = THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial( { color: 0xffffff })
+    const star = new THREE.Mesh( geometry, material );
+
+    const [x, y, z] =Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+
+    star.position.set(x, y, z);
+    scene.add(star)
+}
+
+
 function animate() {
     requestAnimationFrame( animate );
 
