7808430bab635c9fe24794b14de600fb4516b9c5
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-09-12 02:57:27 -0400

    added light helper to scene

--- main.js
+++ main.js
@@ -29,6 +29,8 @@ pointLight.position.set(5,5,5)
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight)
 
+const lightHelper = new THREE.PointLightHelper(pointLight)
+scene.add(lightHelper)
 
 function animate() {
     requestAnimationFrame( animate );
