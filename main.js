279fa8cd321bb533867ece09477b01b0a68a702f
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-09-12 01:44:12 -0400

    created mesh by adding geometry to material

--- main.js
+++ main.js
@@ -19,3 +19,4 @@ renderer.render( scene, camera );
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100 )
 const material = new THREE.MeshBasicMaterial( { color: 0xff6347, wireframe: true } );
+const torus = new THREE.Mesh( geometry, material );
