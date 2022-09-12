30b77ae84ac7762b0f22424ff071d0f67a643ed3
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-09-12 01:46:31 -0400

    added thing to scene with scene.add

--- main.js
+++ main.js
@@ -20,3 +20,7 @@ renderer.render( scene, camera );
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100 )
 const material = new THREE.MeshBasicMaterial( { color: 0xff6347, wireframe: true } );
 const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus)
+
+renderer.render( scene, camera );
