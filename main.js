ffeb4b2286a536757a6326dc431902901f65012e
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-09-12 01:39:32 -0400

    added object geometry torus

--- main.js
+++ main.js
@@ -15,4 +15,4 @@ renderer.setPixelRatio(window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 
-renderer.render( scene, camera )
+renderer.render( scene, camera );
