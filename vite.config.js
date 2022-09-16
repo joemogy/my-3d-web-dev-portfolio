6e8e83e65df67681b46d720ec3431473b3617c8b
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-09-15 23:08:52 -0400

    useless

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-//
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: '/my-3d-web-dev-portfolio',
-    plugins: [vue()],
-});
